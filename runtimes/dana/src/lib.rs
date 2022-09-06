fb929e76d7165791688e3d0b92ec109fc4de9960
ruibinx <xingrobin@gmail.com>
2022-09-06 19:00:47 +0800

    add: run reset height for tag

--- runtimes/dana/src/lib.rs
+++ runtimes/dana/src/lib.rs
@@ -101,6 +101,7 @@ pub type Executive = frame_executive::Executive<
     Runtime,
     AllPalletsWithSystem,
     (
+        parami_tag::migrations::v2::ResetHeight<Runtime>,
         parami_swap::migrations::v2::ResetHeight<Runtime>,
         parami_tag::migrations::v3::AddTagNameMigration<Runtime>,
     ),
